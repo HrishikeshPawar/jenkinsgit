398bc360c7fcd9f7f7cdc4f8ecae6897ebc1a307
HrishikeshPawar <rushipawar197@gmail.com>
2023-04-21 09:11:02 +0530

    new code added

---
+++ pattern.java
@@ -0,0 +1,22 @@
+package assignment1;
+
+public class pattern {
+
+	public static void main(String[] args) {
+		for (int i=65;i<=69;i++)//row
+		{
+			for(int j=65;j<=i;j++)//column
+			{
+				System.out.print((char)j);
+			}
+			System.out.println();
+		}
+
+	}
+
+
+
+
+	
+}
+
